ff49e0a5584744776c4e2fec9aa1c636dba751a8
Lasse <Lasse@LASSES-LAPTOP>
2023-12-31 03:22:54 +0100

    ik snap er niks van maja bon

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -112,6 +112,10 @@ let aap;
         return "'don'tknow";
     }
 }*/
+
+//     --- Scores ---     
+// Chrome: 0.05ms to 0.1ms --> this method is optimal for Chrome!
+// IE11: 0.4ms to 1ms
 function DragCalculator(dialog){ // This is the 4th iteration of optimising the window drag calculations. This is a little bit slower than the previous version but it's far cleaner and more easy to modify so I can add constraints.
     this.dialog = dialog;
     this.offset = dialog.clickOffset;
@@ -201,6 +205,9 @@ aap = {
     }
 }
 
+//   --- Scores ---    
+// Chrome: 0.8ms to 1ms
+// IE11: 0.1ms to 0.5ms --> this previous method is somehow optimal for IE11!
 function DragAction(){ // This looks less elegant than checking on mouse move but if we simply define the function in advance we save quite a lot of performance by doing the resize method calculations in advance instead on every mouse move tick. I also intentionally split the code up again so we do have duplicate code but in this case it's far more efficient to do 1 function call with 0 if statements than doing 16 function calls with 3 * 6 + 2 if statements for each direction on every mousemove event! Even the visually pleasing but technically sluggish method works relatively smoothly on modern browsers, it gets quite horrible once reflections and blur are enabled, these effects are done by native code in the browser and we can't optimise that so I did my best to make this as efficient as I could come up with. Performance is absolutely necessary because we want the window dragging to feel instantaneous, lag is absolutely not tolerated even on slow hardware and deprecated browsers!
     this.execute = function(){},
     this.set = function(direction){ if(!activeDrag) activeDrag = true, this.execute = this.resizeFunctions[direction] || function(){} },
