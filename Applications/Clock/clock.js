15ed6f81a7d5d3065441184188affde5ac4087e2
Lasse <Lasse@LASSES-LAPTOP>
2024-01-09 03:28:39 +0100

    Hoe kan ik dat eigenlijk ik heb da gewoon in mijn hoofd bedacht en dat werkt ook gewoon zomaar

--- Applications/Clock/clock.js
+++ Applications/Clock/clock.js
@@ -7,15 +7,6 @@ const akka = displayBuilder.build();
 document.body.appendChild(akka);
 
 document.getElementById("size").onchange = function(){
-    akka.querySelectorAll("div.segmentx").forEach(function(element){
-        //element.style.backgroundColor = "orange";
-        //element.style.borderColor = "orange";
-        element.style.borderBottomColor = "orange";
-    });
-    akka.querySelectorAll("div.segmentx:after").forEach(function(element){
-        element.style.borderTopColor = "red";
-        element.style.opacity = "0.3";
-    });
     akka.querySelectorAll("div.segmentdisplay>div").forEach(function(element){
         element.style.borderColor = "red";
     });
--- Applications/Clock/displaybuilder.js
+++ Applications/Clock/displaybuilder.js
@@ -1,19 +1,46 @@
 
 function DisplayBuilder(number){
-
+    this.display;
+    this.number= number || 0;
+    this.segments = [];
 }
 
+displayNumbers = [
+    [true, true, true, false, true, true, true],
+    [false, false, true, false, false, true, false],
+    [true, false, true, true, true, false, true],
+    [true, false, true, true, false, true, true],
+    [false, true, true, true, false, true, false],
+    [true, true, false, true, false, true, true],
+    [true, true, false, true, true, true, true],
+    [true, true, true, false, false, true, false],
+    [true, true, true, true, true, true, true],
+    [true, true, true, true, false, true, true],
+]
+
 DisplayBuilder.prototype = {
+    number: 1,
     build: function(){
         const display = document.createElement("div");
         display.classList.add("segmentdisplay");
         for (let i = 0; i < 7; i++) {
             const segment = display.appendChild(document.createElement("div"));
+            this.segments.push(segment);
             if(i==0 || i==3 || i==6) segment.classList.add("segmentx");
             if(i==1 || i==2 || i==4 || i==5) segment.classList.add("segmenty");
             if(i==2 || i==5) segment.classList.add("segmentr");
             if(i==3 || i==6) segment.classList.add("segmenth");
         }
         return display;
+    },
+    update: function(number){
+        //let i = 0;
+        this.segments.forEach(function(segment, index){
+            //i++
+            console.log(displayNumbers, this.number)
+            if(!displayNumbers[number || 0][index]) {
+                segment.style.opacity = "0";
+            } else segment.style.opacity = "1";
+        });
     }
 }
