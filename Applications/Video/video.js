ceff103e9973f2c08448b8afb1134030cf88fb44
Lasse <Lasse@LASSES-LAPTOP>
2024-01-09 17:14:03 +0100

    ze zaagt ja

--- Applications/Video/video.js
+++ Applications/Video/video.js
@@ -1,4 +1,14 @@
 
+const youtubeFrame = document.getElementById("youtube");
+
 document.onload = function(ev){
     console.log("I DID A THINGK", ev);
+    const location = new URL(window.location);
+    const type = location.searchParams.get("type");
+    switch(type){
+        case "youtube":
+            const youtube = new YouTubeParser(location.searchParams.get("url"));
+            youtubeFrame.url = youtube.embedURL.href;
+        break;
+    }
 }
--- Scripts/youtube.js
+++ Scripts/youtube.js
@@ -1,4 +1,20 @@
 
 function YouTubeParser(url){
-    this.url = 
+    this.url = url;
+    //this._embedURL = new URL(this.url);
+}
+
+YouTubeParser.prototype.getEmbedURL = function(){
+    "https://www.youtube.com/watch?v=2Ni13dnAbSA"
+    "https://www.youtube.com/embed/2Ni13dnAbSA"
+    "https://www.youtube.com/embed/tgbNymZ7vqY"
+
+}
+YouTubeParser.prototype = {
+    get embedURL(){
+        const url = new URL(this.url);
+        url.pathname = "/embed/" + url.searchParams.get("v");
+        url.searchParams.delete("v");
+        return url;
+    }
 }
