1799929bfe8e58e2748446d653b32cb159b773a3
Lasse <Lasse@LASSES-LAPTOP>
2024-01-10 17:14:17 +0100

    ey

--- Applications/Music/music.js
+++ Applications/Music/music.js
@@ -1,12 +1,16 @@
 
 const micButton = document.getElementById("mic");
 const virtualAudio = document.createElement("audio");
+const constext = new AudioContext;
+const audioAnalyser = new audioAnalyser;
 
 micButton.onclick = function(ev){
-    navigator.getUserMedia({audio: true, video: false}, e=>console.log(e), e=>console.log(e))
+    navigator.getUserMedia({audio: true, video: false}, e=>microphoneActivated(e), e=>console.error(e.message))
 
 }
 
 function microphoneActivated(stream){
-
+    //virtualAudio.srcObject = stream;
+    constext.createMediaStreamSource(stream).connect(audioAnalyser);
+    audioAnalyser.connect(constext.destination);
 }
--- Applications/Music/visualiser.js
+++ Applications/Music/visualiser.js
@@ -8,19 +8,19 @@ file.onchange = function(ev){
     console.log("File received!", ev);
     audio.src = URL.createObjectURL(this.files[0]);
     const audioContext = new AudioContext(),
-        src = audioContext.createMediaElementSource(audio),
+        source = audioContext.createMediaElementSource(audio),
         //src = audioContext.createMediaStreamSource()
         analyser = audioContext.createAnalyser();
     src.connect(analyser);
-    analyser.connect(audioContext.destination);
+    
     doAnalStuff(analyser);
 }
 
-function doAnalStuff(analyser){
+function doAnalStuff(analyser, context){
+
+    analyser.connect(audioContext.destination);
     analyser.fftSize = 32;
-    const bufferLength = analyser.frequencyBinCount;
-    console.log(bufferLength);
-    const dataArray = new Uint8Array(bufferLength);
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(dataArray);
 
     for (var i = 0; i < bufferLength; i++) console.log(barHeight = dataArray[i])
