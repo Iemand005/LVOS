f4b249c3cd8da9cb54fa3c3c0c60315b6f5c48db
Lasse <Lasse@LASSES-LAPTOP>
2024-01-10 17:36:29 +0100

    elaba

--- Applications/Music/visualiser.js
+++ Applications/Music/visualiser.js
@@ -1,6 +1,31 @@
 
 // document.appendChild(document.getElementById("file"))
 
+function AudioVisualiser(){
+    this.context = new AudioContext;
+    this.analyser = this.context.createAnalyser();
+    // this.analyser.connect(.destination);
+    this.source;
+    this.data;
+}
+
+AudioVisualiser.prototype = {
+    initializeWithMediaElement: function(element){
+        this.source = this.analyser.context.createMediaElementSource(element);
+    },
+
+    initializeWithMediaStream: function(stream){
+        this.source = this.analyser.context.createMediaStreamSource(stream);
+    },
+    startAnalyser: function(fttSize){
+        this.source.connect(this.analyser);
+        
+        this.analyser.fftSize = fttSize || 64;
+        this.data = new Uint8Array(this.analyser.frequencyBinCount);
+        this.analyser.getByteFrequencyData(this.data);
+    }
+}
+
 const file = document.getElementById("file");
 const audio = document.getElementsByTagName("audio")[0];
 
@@ -9,15 +34,15 @@ file.onchange = function(ev){
     audio.src = URL.createObjectURL(this.files[0]);
     const audioContext = new AudioContext(),
         source = audioContext.createMediaElementSource(audio),
-        //src = audioContext.createMediaStreamSource()
         analyser = audioContext.createAnalyser();
-    src.connect(analyser);
     
-    doAnalStuff(analyser);
+    
+    doAnalStuff(analyser, source);
 }
 
-function doAnalStuff(analyser, context){
+function doAnalStuff(analyser, source){
 
+    src.connect(analyser);
     analyser.connect(audioContext.destination);
     analyser.fftSize = 32;
     const dataArray = new Uint8Array(analyser.frequencyBinCount);
