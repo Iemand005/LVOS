3d153a1deebd263e8c13400074a29d500bd1c50b
Lasse <Lasse@LASSES-LAPTOP>
2024-01-10 20:09:54 +0100

    damn an d the calculation is right first try wtf even

--- Applications/Music/music.js
+++ Applications/Music/music.js
@@ -1,4 +1,6 @@
 
+const frequencies = 128;
+
 const micButton = document.getElementById("mic");
 const virtualAudio = document.createElement("audio");
 const file = document.getElementById("file");
@@ -12,44 +14,27 @@ const elements = [];
  
 
 function animateFrame(eudioVisualiser){
-    //window.setTimeout(new Function(), 1000);
     requestAnimationFrame(animateFrame.bind(this, eudioVisualiser));
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = "red"
+
     for(let index in eudioVisualiser.data){
-        //elements[index].innerText = eudioVisualiser.data[index];
-        const amp = eudioVisualiser.data[index];
+        const amp = parseInt(eudioVisualiser.data[index]);
         const count = eudioVisualiser.frequencyBinCount;
-        const x = index * (ctx.canvas.width/count);
-        const x1 = (index+1) * (ctx.canvas.width/count);
-        ctx.fillStyle = "red"
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-// console.log(x*index)
+        const width = ctx.canvas.width/count;
+        const x = index * width;
         ctx.beginPath();
-        
-        ctx.fillRect(0, 10, 10, 10);
-        ctx.fillText(amp, 10, 10);
-        ctx.fillStyle = "green"
-        ctx.fillRect(10, 10, 10, amp*10 + 30);
+        ctx.fillRect(x, ctx.canvas.height, width, -(ctx.canvas.height/256 *amp));
         ctx.fill();
         ctx.closePath();
     }
-    
-    //console.log(e)
 }
-
-//setInterval(animateFrame, 1000);
 
 file.onchange = function(ev){
     audio.src = URL.createObjectURL(this.files[0]);
     audio.load();
     audio.play();
-    const eudioVisualiser = new AudioVisualiser(32);
-    /*for (let i = 0; i < eudioVisualiser.frequencyBinCount; i++) {
-        const element = document.createElement("p");
-        elements.push(element);
-        element.innerText = "hey"
-        document.body.appendChild(element);
-    }*/
+    const eudioVisualiser = new AudioVisualiser(frequencies);
     eudioVisualiser.initializeWithMediaElement(audio);
     animateFrame(eudioVisualiser);
-    //setInterval(animateFrame, 1000, eudioVisualiser);
 }
