70cdf0b395c04b44dd28e8369acba91f5a351778
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-09 01:11:14 +0100

    AUdio visualizer fixes

--- Applications/Music/visualiser.js
+++ Applications/Music/visualiser.js
@@ -5,8 +5,7 @@
 'use strict';
 
 function AudioVisualiser(fftSize){
-    this.context = new AudioContext(); // The brackets in a constructor without parameters aren't required anymore. I write them here for compatibility.
-    this.analyser = this.context.createAnalyser();
+    this.preInit();
     this.elementSource;
     this.streamSource;
     this.updateBinCount(fftSize);
@@ -22,34 +21,43 @@ AudioVisualiser.prototype.disconnectAnalyser = function () {
         if (this.analyser && this.analyser.disconnect) this.analyser.disconnect();
     };
 
+AudioVisualiser.prototype.preInit = function () {
+    this.context = new AudioContext();
+    this.analyser = this.context.createAnalyser();
+};
+
+AudioVisualiser.prototype.initialize = function (source) {
+    this.source = source;
+    this.source.connect(this.analyser);
+    this.analyser.connect(this.context.destination);
+}
+
 AudioVisualiser.prototype.initializeWithMediaElement = function (element) {
-        this.source = this.context.createMediaElementSource(element);
-        this.source.connect(this.analyser);
-        this.analyser.connect(this.context.destination);
-    };
+    this.preInit();
+    this.initialize(this.context.createMediaElementSource(element));
+};
 
 AudioVisualiser.prototype.initializeWithMediaStream = function (stream) {
-        this.source = this.context.createMediaStreamSource(stream);
-        this.source.connect(this.analyser);
-        this.analyser.connect(this.context.destination);
-    };
+    this.preInit();
+    this.initialize(this.context.createMediaStreamSource(stream));
+};
 
 AudioVisualiser.prototype.updateBinCount = function (fftSize) {
         this.analyser.fftSize = fftSize || 64;
         this._frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
         this._timeDomainData = new Uint8Array(this.analyser.frequencyBinCount);
     },
-    Object.defineProperty(Geode.prototype, "frequencyBinCount", {
+    Object.defineProperty(AudioVisualiser.prototype, "frequencyBinCount", {
   get: function() {
         return this.analyser.frequencyBinCount;
     }
 });
-    Object.defineProperty(Geode.prototype, "frequencyData", {
+    Object.defineProperty(AudioVisualiser.prototype, "frequencyData", {
     get: function() {
         return this.analyser.getByteFrequencyData(this._frequencyData), this._frequencyData;
     },
 });
-    Object.defineProperty(Geode.prototype, "timeDomainData", {
+    Object.defineProperty(AudioVisualiser.prototype, "timeDomainData", {
     get: function() {
         return this.analyser.getByteTimeDomainData(this._timeDomainData), this._timeDomainData;
     }
