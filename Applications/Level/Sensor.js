26152f36acdeb9374fa4875673541629261f8106
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-15 19:44:16 +0100

    yup we do it again

--- Applications/Level/Sensor.js
+++ Applications/Level/Sensor.js
@@ -1,3 +1,11 @@
+ // Sensors
+//  Lasse Lauwerys (2026) c
+
+
+'use strict';
+'use esnext';
+'use moz';
+
 if (ondevicemotion) {
   ondevicemotion = function(e) {
     console.log("Acceleration:", e.acceleration, e.accelerationIncludingGravity);
