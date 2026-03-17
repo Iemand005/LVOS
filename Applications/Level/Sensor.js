0d59fa4e48da8cf74b53a969ef303241c4844805
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-17 19:05:33 +0100

    hrm

--- Applications/Level/Sensor.js
+++ Applications/Level/Sensor.js
@@ -36,57 +36,61 @@ function Graphics2D(element) {
 
 function drawHorizon(roll, pitch) {
   var w = horizon.width, h = horizon.height;
-  var size = Math.max(w, h) * 4;
+
+  const pOffset = pitch * 5;
 
   ctx.clearRect(0, 0, w, h);
-  
   ctx.save();
+
   ctx.translate(w / 2, h / 2);
-  ctx.rotate(roll);
+  ctx.rotate(roll * Math.PI / 180);
 
-  var pitchOffset = -(pitch / (Math.PI / 2)) * (h / 2);
+  ctx.fillStyle = "#3498db";
+  ctx.fillRect(-w * 2, -h * 2 - pOffset, w * 4, h * 2);
 
-  ctx.beginPath();
-  ctx.fillStyle = "skyblue";
-  ctx.arc(0, pitchOffset - size, size, 0, Math.PI * 2);
-  ctx.fill();
+  ctx.fillStyle = "#8b4513";
+  ctx.fillRect(-w * 2, 0 - pOffset, w * 4, h * 2);
 
-  ctx.beginPath();
-  ctx.fillStyle = "green";
-  ctx.arc(0, pitchOffset + size, size, 0, Math.PI * 2);
-  ctx.fill();
-
-  ctx.beginPath();
   ctx.strokeStyle = "white";
   ctx.lineWidth = 2;
-  ctx.moveTo(-size, pitchOffset);
-  ctx.lineTo(size, pitchOffset);
+  ctx.beginPath();
+  ctx.moveTo(-w, -pOffset);
+  ctx.lineTo(w, -pOffset);
   ctx.stroke();
+
   ctx.restore();
 
+  ctx.strokeStyle = "yellow";
+  ctx.lineWidth = 4;
   ctx.beginPath();
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 3;
-  ctx.moveTo(w / 2 - 20, h / 2);
-  ctx.lineTo(w / 2 + 20, h / 2);
-  ctx.stroke();
-  
-  ctx.beginPath();
-  ctx.moveTo(w / 2, h / 2 - 20);
-  ctx.lineTo(w / 2, h / 2 + 20);
+  ctx.moveTo(w / 2 - 40, h / 2);
+  ctx.lineTo(w / 2 - 10, h / 2);
+  ctx.moveTo(w / 2 + 10, h / 2);
+  ctx.lineTo(w / 2 + 40, h / 2);
   ctx.stroke();
 }
 
-if (typeof ondevicemotion !== "undefined") {
-  ondevicemotion = function(e) {
-    var g = e.accelerationIncludingGravity;
-    if (!g || g.x === null) return;
-     
-    var roll = Math.atan2(g.x, g.y);
-    var pitch = Math.atan2(g.z, Math.sqrt(g.x * g.x + g.y * g.y));
+function handleMotion(e) {
+  var g = e.accelerationIncludingGravity;
+  if (!g || g.x === null) return;
+  var x = g.x, y = g.y, z = g.z;
+    
+  // var roll = Math.atan2(g.x, g.y);
+  // var pitch = Math.atan2(-g.z, Math.sqrt(g.x * g.x + g.y * g.y));
 
-    drawHorizon(roll, pitch);
-  }
+  const pitch = Math.atan2(y, Math.sqrt(x * x + z * z)) * (180 / Math.PI);
+  const roll = Math.atan2(-x, z) * (180 / Math.PI);
+
+  drawHorizon(roll, pitch);
+}
+
+function startLevel() {
+  if (typeof DeviceMotionEvent.requestPermission === 'function')
+    DeviceMotionEvent.requestPermission().then(function (permission) {
+      if (permission === 'granted')
+        window.addEventListener('devicemotion', handleMotion);
+    });
+  else window.addEventListener('devicemotion', handleMotion);
 }
 
 function resize() {
@@ -96,3 +100,5 @@ function resize() {
 
 onresize = resize;
 resize();
+
+startLevel();
