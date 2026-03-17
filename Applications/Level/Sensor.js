335c217f0b9ce474793d81062b3fb5ab49b36905
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-17 13:52:35 +0100

    normalizing

--- Applications/Level/Sensor.js
+++ Applications/Level/Sensor.js
@@ -9,6 +9,11 @@
 if (typeof ondevicemotion !== "undefined") {
   ondevicemotion = function(e) {
     // console.log("Acceleration:", e.acceleration, e.accelerationIncludingGravity);
-    Math.cos(e.x)
+    // Math.cos(e.x)
+    // console.log("ee", e.accelerationIncludingGravity);
+    var acceleration = new Vector3D(e.accelerationIncludingGravity.x, e.accelerationIncludingGravity.y, e.accelerationIncludingGravity.z);
+    console.log(" ae", acceleration)
+    acceleration.normalize();
+    console.log("Acceleration norma:", acceleration);
   }
 }
