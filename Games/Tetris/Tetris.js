d7ce699e5706495374be6488ba2b731930adf0ea
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-18 23:11:11 +0100

    fwart fart twat ass hole

--- Games/Tetris/Tetris.js
+++ Games/Tetris/Tetris.js
@@ -2,12 +2,13 @@
 function Tetris() {
   this.table = document.createElement("table");
   this.rows = [];
+  this.blocks = [];
 }
 
 Tetris.prototype.init = function() {
   document.body.appendChild(this.table);
 
-  this.createGrid(1,1);
+  this.createGrid(2, 2);
 };
 
 Tetris.prototype.createGrid = function(width, height) {
@@ -17,7 +18,8 @@ Tetris.prototype.createGrid = function(width, height) {
 
     for (var x = 0; x < width; x++) {
       var data = row.appendChild(document.createElement("td"));
-      data.textContent = "hey";
+      // data.textContent = "hey";
+      data.classList.add("hero");
     }
 
     this.rows.push(row);
