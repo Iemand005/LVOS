4d808b409e5cc6176828fa68aec5ba83ff74e9d0
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-18 22:57:41 +0100

    mhmph

--- Games/Tetris/Tetris.js
+++ Games/Tetris/Tetris.js
@@ -1,14 +1,29 @@
 
 function Tetris() {
-
   this.table = document.createElement("table");
+  this.rows = [];
 }
 
 Tetris.prototype.init = function() {
   document.body.appendChild(this.table);
+
+  this.createGrid(1,1);
 };
 
-Tetris.prototype.render = function()
+Tetris.prototype.createGrid = function(width, height) {
+  
+  for (var y = 0; y < height; y++) {
+    var row = this.table.appendChild(document.createElement("tr"));
+
+    for (var x = 0; x < width; x++) {
+      var data = row.appendChild(document.createElement("td"));
+      data.textContent = "hey";
+    }
+
+    this.rows.push(row);
+  }
+
+}
 
 var tetris = new Tetris();
 
