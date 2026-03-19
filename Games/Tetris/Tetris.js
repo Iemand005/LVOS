fee3050f72325f40c51ff9d47737ed92d72c6d21
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-19 19:23:46 +0100

    Better then

--- Games/Tetris/Tetris.js
+++ Games/Tetris/Tetris.js
@@ -1,4 +1,8 @@
 
+/**
+ * @typedef {"hero" | "smashboy" | "teewee" | "rhode-island-z" | "cleveland-z" | "blue-ricky" | "orange-ricky" } TetrominoType
+ */
+
 function Tetris() {
   this.table = document.createElement("table");
   this.rows = [];
@@ -8,7 +12,7 @@ function Tetris() {
 Tetris.prototype.init = function() {
   document.body.appendChild(this.table);
 
-  this.createGrid(10, 2);
+  this.createGrid(6, 10);
 };
 
 Tetris.prototype.createGrid = function(width, height) {
@@ -18,26 +22,13 @@ Tetris.prototype.createGrid = function(width, height) {
 
     for (var x = 0; x < width; x++) {
       var data = row.appendChild(document.createElement("td"));
-      data.classList.add("hero");
+      // data.classList.add("hero");
     }
 
     this.rows.push(row);
   }
 
 }
-
-/**
- * @typedef {"hero" | "smashboy" | "teewee" | "rhode-island-z" | "cleveland-z" | "blue-ricky" | "orange-ricky" } TetrominoType
- */
-
-var
-hero = [
-  [1, 1],
-  [1, 1]
-],
-smashboy = [
-
-]
 
 /**
  * @param {TetrominoType} type 
@@ -81,6 +72,11 @@ Tetris.prototype.getTetrominoLayout = function(type) {
  */
 Tetris.prototype.spawn = function(type) {
   var layout = this.getTetrominoLayout();
+
+  // for (let index = 0; index < array.length; index++) {
+  //   const element = array[index];
+    
+  // }
 }
 
 var tetris = new Tetris();
