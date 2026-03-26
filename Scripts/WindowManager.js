07a782b5b284c4084f8cac4a84d64e29092b7abe
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-26 10:03:01 +0100

    Cleanup

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -532,7 +532,7 @@ function DragAction(){ // This looks less elegant than checking on mouse move bu
     this.execute = function(){};
     /** @type {DragFunction[]} */
     this.resizeFunctions = [
-        function(dialog, offset, difference){ dialog.move(offset.left + difference.x, offset.top + difference.y); console.log("Moving") }, // Move
+        function(dialog, offset, difference){ dialog.move(offset.left + difference.x, offset.top + difference.y); }, // Move
         function(dialog, offset, difference){ dialog.top = offset.top + difference.y }, // Top
         function(dialog, offset, difference){ dialog.width = offset.width + difference.x }, // Right
         function(dialog, offset, difference){ dialog.height = offset.height + difference.y }, // Bottom
