38b88b34c6596878f853f596001ff76db4aa5109
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-29 15:55:58 +0200

    yea i's a bug man

--- undefined.js
+++ undefined.js
@@ -2,21 +2,30 @@
 
 function Test() {
   this._x = 0;
-  this.y = 0;
+  this._y = 0;
+  this.z = 0;
 }
 
 Object.defineProperty(Test.prototype, "x", {
     get: function() { return this._x; },
     set: function(x) { return this._x = x; }
-}); 
+});
+
+Object.defineProperty(Test.prototype, "y", {
+    get: function() { return this._y; },
+    set: function(y) { if (typeof y === "number") return this._y = y; }
+});
 
 Test.prototype.read = function() {
   /** @type {number} */
   var a = this.x;
   /** @type {number} */
   var b = this.y;
+  /** @type {number} */
+  var c = this.z;
 }
 
 Test.prototype.write = function() {
-    this.y = 10;
+  this.y = 10;
+  this.z = 10;
 }
