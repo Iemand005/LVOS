b1974cc3cc355d4a7a1ecdc455e1437669762146
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-03-29 15:51:37 +0200

    This is the bug

--- undefined.js
+++ undefined.js
@@ -2,17 +2,19 @@
 
 function Test() {
   this._x = 0;
+  this.y = 0;
 }
 
 Object.defineProperty(Test.prototype, "x", {
-    get: function() { return this._x; }
+    get: function() { return this._x; },
+    set: function(x) { return this._x = x; }
 }); 
 
 Test.prototype.read = function() {
   /** @type {number} */
   var a = this.x;
   /** @type {number} */
-  var v = this.y;
+  var b = this.y;
 }
 
 Test.prototype.write = function() {
