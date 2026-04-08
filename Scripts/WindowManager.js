755b9c307ab6034ff387c8bac6c02ea07726b441
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-08 21:11:59 +0200

    Tell me why

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -346,7 +346,7 @@ Dialog.prototype.initWithObject = function(object) {
  * @param {number} [b] 
  */
 function max(a, b) {
-    if (a || 0 > (b || 0)) return a || 0;
+    if ((a || 0) > (b || 0)) return a || 0;
     else return b|| 0;
 }
 
