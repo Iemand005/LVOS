7c00e784da088f24f544080011416d411da1e7c9
Foxyz <lauweryslasse@gmail.com>
2026-05-25 10:59:41 +0200

    nino

--- Scripts/sw.js
+++ Scripts/sw.js
@@ -4,15 +4,15 @@ const ASSETS = [
   '/index.html'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
