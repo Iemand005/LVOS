d6536214eeaa27d276d8559d0f3083b71f92ca87
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-01 20:37:18 +0200

    make functional

--- Scripts/LVOSLoader.js
+++ Scripts/LVOSLoader.js
@@ -1,7 +1,20 @@
 
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = 'styles.css';
 
-document.head.appendChild(link);
+function loadStyleSheet(url) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = url;
+  
+  document.head.appendChild(link);
+
+}
+
+function loadScript(url, callback) {
+  const script = document.createElement('script');
+  script.src = url;
+  
+  script.onload = callback;
+  
+  document.head.appendChild(script);
+}
