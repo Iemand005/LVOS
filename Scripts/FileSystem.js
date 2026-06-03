b78e34c6eb6ecbabd48e00c6243d79bf0e108dba
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-03 18:53:32 +0200

    no way

--- Scripts/FileSystem.js
+++ Scripts/FileSystem.js
@@ -2,5 +2,23 @@
 /** @typedef ""  */
 
 function FileSystem() {
+	if (!window.webkitRequestFileSystem) {
+      return reject('webkitRequestFileSystem not supported here.');
+    }
 
+    const size = 5 * 1024 * 1024;
+    
+    window.webkitRequestFileSystem(window.TEMPORARY, size, function(fs) {
+      fs.root.getFile(fileName, { create: true }, function(fileEntry) {
+        fileEntry.createWriter(function(fileWriter) {
+          
+          fileWriter.onwriteend = () => resolve(`Saved via Chrome Legacy FS to ${fileEntry.toURL()}`);
+          fileWriter.onerror = (e) => reject(e);
+          
+          const blob = new Blob([textData], { type: 'text/plain' });
+          fileWriter.write(blob);
+          
+        }, reject);
+      }, reject);
+    }, reject);
 }
