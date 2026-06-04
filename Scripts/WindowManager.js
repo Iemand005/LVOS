5db4a552ddfa37c25c71c28fd2aeeac46b6930ae
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-04 14:07:09 +0200

    mica uhm i needa fixxeee him

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -943,7 +943,7 @@ function getWallpaper() {
 
 Dialog.prototype.injectMica = function() {
     try {
-        if (!this.useTransform) return;
+        if (!this.useTransform) console.warn("Dude you still gotta fix the mica here for oh right but can you psosible even do that??");
         if (this.micaElement || !this.target) return;
         var wallpaper = document.getElementById("wallpaper");
         if (!wallpaper) return;
