549a50a0d52e5b73d27d145ff683d806bc1c0a6d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-06 14:55:55 +0200

    fix applist prevent default

--- Scripts/DesktopManager.js
+++ Scripts/DesktopManager.js
@@ -8,19 +8,22 @@
 
 /** @typedef {import(./WindowManager.js).Dialog} Dialog */
 
-var applist = document.getElementById("applist");
-
-applist.addEventListener("submit", function(event){ event.preventDefault(); });
+document.addEventListener("load", function() {
+    var applist = document.getElementById("applist");
+    applist.addEventListener("submit", function(event){
+        event.preventDefault();
+    });
+})
 
 var reflecitons = false;
 
-var windowr = document.getElementById("windows");
 var dock = document.getElementById("dock");
 var reflectionr = document.getElementById("reflection");
 var reflector = new Reflector(document.getElementById("reflection"));
 var applistItems = document.getElementById("dockapplist");
 
 function toggleReflections(force) {
+    if (!reflector) return;
     if(force === null) reflecitons = !reflecitons;
     else reflecitons = Boolean(force);
     if(reflecitons) windowManager.forEachWindow(function(dialog) { reflector.reflect(dialog.target); });
