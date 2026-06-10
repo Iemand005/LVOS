dabbbef0a0adc118bbfa29097422d78722a98973
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-10 22:49:48 +0200

    nt needed?

--- Applications/RenderDonkey/renderfox.js
+++ Applications/RenderDonkey/renderfox.js
@@ -83,13 +83,6 @@ const buffers = {
   color: colorBuffer
 };
 
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
-gl.bufferData(
-  gl.ELEMENT_ARRAY_BUFFER,
-  new Uint16Array(indices),
-  gl.STATIC_DRAW
-);
-
 graphics.buffers = buffers;
 
 graphics.resize();
@@ -100,3 +93,66 @@ console.log(graphics);
 window.onresize = function (ev) {
   graphics.resize();
 };
+
+// var gl = canvas.getContext("webgl");
+
+function resize()
+{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+}
+window.onresize = resize;
+resize();
+
+// Vertex shader (fullscreen triangle)
+var vs =
+"attribute vec2 p;" +
+"void main(){" +
+"gl_Position=vec4(p,0.0,1.0);" +
+"}";
+
+// Fragment shader
+var fs =
+"precision mediump float;" +
+"void main(){" +
+"gl_FragColor=vec4(0.2,0.6,1.0,1.0);" +
+"}";
+
+function compile(type, src)
+{
+    var s = gl.createShader(type);
+    gl.shaderSource(s, src);
+    gl.compileShader(s);
+    return s;
+}
+
+var program = gl.createProgram();
+gl.attachShader(program, compile(gl.VERTEX_SHADER, vs));
+gl.attachShader(program, compile(gl.FRAGMENT_SHADER, fs));
+gl.linkProgram(program);
+gl.useProgram(program);
+
+// Fullscreen triangle (NO buffers, just 1 attribute)
+var buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+// fullscreen triangle
+var vertices = new Float32Array([
+    -1, -1,
+     3, -1,
+    -1,  3
+]);
+
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+var loc = gl.getAttribLocation(program, "p");
+gl.enableVertexAttribArray(loc);
+gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
+
+function draw()
+{
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    requestAnimationFrame(draw);
+}
+draw();
