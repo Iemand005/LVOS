7b3b64564c2b5998904391eb7ef44bb3cc534010
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-10 20:19:38 +0200

    the completions are ncie here

--- Scripts/Console.js
+++ Scripts/Console.js
@@ -28,6 +28,13 @@ ConsoleInterceptor.prototype.intercept = function() {
 	try {
 	if (this.stdout.firstChild) this.stdout.removeChild(this.stdout.firstChild);
 	var html = this.getHTML();
+	html.style.background = "purple"
+	html.style.display = "block";
+	html.style.position = "absolute";
+	html.style.left = "0";
+	html.style.top = "0";
+	html.style.width = "100%";
+	html.style.height = "100%";
 	alert(html)
 	alert (this.stdout)
     this.stdout.appendChild(html);
