d09a6f4fe43ddc84e2c7a488df232cb731798bba
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-10 21:19:51 +0200

    inpup

--- Scripts/Console.js
+++ Scripts/Console.js
@@ -45,12 +45,12 @@ ConsoleInterceptor.prototype.intercept = function() {
 
 ConsoleInterceptor.prototype.init = function() {
 	var self = this;
-	
-	var consoleForm = windowManager && windowManager.windows && windowManager.windows["console"] &&windowManager.windows["console"].originalBody || document.getElementById("console").getElementsByTagName("form")[0]; //consoleElement.getElementsByTagName("form")[0];
+	//windowManager && windowManager.windows && windowManager.windows["console"] &&windowManager.windows["console"].originalBody ||
+	var consoleForm =  document.getElementById("console").getElementsByTagName("form")[0]; //consoleElement.getElementsByTagName("form")[0];
 	alert(consoleForm)
 	var stdout = self.stdout = consoleForm.stdout || consoleForm.getElementsByTagName("output")[0] || document.getElementById("stdout");
 	/** @type {HTMLInputElement?} */
-	var stdin = consoleForm.stdin || consoleForm.getElementById("stdin");
+	var stdin = consoleForm.stdin ||consoleForm.getElementsByTagName("INPUT")[0] || consoleForm.getElementById("stdin");
 
 	var interceptConsole = function() { self.intercept(); };
 
