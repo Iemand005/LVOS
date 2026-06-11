7dc6ba27c1373b5a26fadb1570c744b2df8f3ab5
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-11 19:37:32 +0200

    make optinals yeah handy you can slect and press brakc

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -958,10 +958,10 @@ function setClass(element, className, enabled) {
 /**
  * 
  * @param {string} className 
- * @param {boolean} force 
- * @param {string} animationEndTrigger 
- * @param {()=>void} onEnd 
- * @param {(this:Dialog,enabled:boolean)=>void} onToggled 
+ * @param {boolean} [force] 
+ * @param {string} [animationEndTrigger] 
+ * @param {()=>void} [onEnd] 
+ * @param {(this:Dialog,enabled:boolean)=>void} [onToggled] 
  * @returns 
  */
 Dialog.prototype.toggleClassAnimated = function (className, force, animationEndTrigger, onEnd, onToggled) {
@@ -989,8 +989,7 @@ Dialog.prototype.toggleClassAnimated = function (className, force, animationEndT
 /** @param {boolean} [enable] */
 Dialog.prototype.toggleFullScreen = function (enable) {
     if (supportsTransitions) this.toggleClassAnimated("fullscreen", enable, null, null, function(enabled) {
-        if (!this.useTransform) return;
-		if (!this.target) return;
+        if (!this.useTransform || !this.target) return;
         this.target.style.minWidth = enabled ? "100%" : toPixels(this.minWidth);
         this.target.style.minHeight = enabled ? "100%" : toPixels(this.minHeight);
     });
