bdf189eb2e804a4d2f5542b4acd34e87e8262e1d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-11 20:18:35 +0200

    that fixes it

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -242,8 +242,7 @@ function ClickOffset() {
 			this.last = Date.now();
 			this.position.x = x, this.position.y = y;
 			this.positions.push(this.position.clone());
-			this.lastPosition = this.positions.shift() || new Vector;
-			this.difference = ().clone().sub(this.position);
+			this.difference = (this.lastPosition = this.positions.shift() || new Vector).clone().sub(this.position);
 			return this;
 		}
 	};
