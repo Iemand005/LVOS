9ec5a50e41fb5b35bd2a416f2cc8f5734ab9ab5e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-12 23:43:07 +0200

    window manager all errors resolved and types fixed

--- overrides.d.ts
+++ overrides.d.ts
@@ -8,6 +8,10 @@ declare global {
 		__LVMessengerReceive: (type: MessageType, data: any, source: string) => void;
     }
 
+	interface Document {
+        documentMode?: number;
+	}
+
 	interface HTMLElementEventMap {
 		webkitTransitionEnd: TransitionEvent;
 		webkitAnimationEnd: AnimationEvent;
