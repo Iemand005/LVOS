83a5bb0a144224e197daa6cd8818cc31f3cc30ae
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-12 23:24:02 +0200

    cool myd esiree

--- overrides.d.ts
+++ overrides.d.ts
@@ -2,6 +2,6 @@ export {};
 
 declare global {
     interface Window {
-        __LVMessengerReceive: (message: string) => void;
+        __LVMessengerReceive: (message: MessageType) => void;
     }
 }
