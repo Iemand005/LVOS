1e7834a4a2e375a45f7700c93bc6734fed3f3746
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-12 23:42:22 +0200

    IE types add

--- overrides.d.ts
+++ overrides.d.ts
@@ -3,6 +3,10 @@ export {};
 declare global {
     interface Window {
         __LVMessengerReceive: (type: MessageType, data: any, source: string) => void;
+		 interface Window {
+        MSInputMethodContext?: unknown;
+        documentMode?: number;
+    }
     }
 
 	interface HTMLElementEventMap {
