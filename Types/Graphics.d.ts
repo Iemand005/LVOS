6baf92572b4970efe2730d79920cd15d55a070ad
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-18 01:42:21 +0200

    remvoe huh it's tnot type augmenting

--- Types/Graphics.d.ts
+++ Types/Graphics.d.ts
@@ -4,9 +4,4 @@ declare global {
     interface Graphics3D {
         loadShader(type: number, source: string): WebGLShader | null;
     }
-
-    var Graphics3D: {
-        new (): Graphics3D;
-        prototype: Graphics3D;
-    };
 }
