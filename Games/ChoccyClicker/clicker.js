4ce813ef6e0b0e9e3bbfbb7cc565e50a35a747e1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-19 17:13:23 +0200

    textnode

--- Games/ChoccyClicker/clicker.js
+++ Games/ChoccyClicker/clicker.js
@@ -1,5 +1,7 @@
 
 var button = document.getElementById("button");
+var clickCount = document.getElementById("click-count");
+
 
 function Clicker() {
 	this.clicks = 0;
@@ -7,6 +9,9 @@ function Clicker() {
 
 var clicker = new Clicker();
 
+var textNode = document.createTextNode("0");
+
+if (clickCount) clickCount.appendChild(textNode);
 if (button) button.onclick = function() {
 	clicker.clicks++;
 };
