5bff4a63634f417f64100e726d5c56452357d311
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-19 13:21:11 +0200

    attitude

--- Games/Sudoku/sudoku.js
+++ Games/Sudoku/sudoku.js
@@ -11,7 +11,17 @@ function Grid(width, height) {
 }
 
 Grid.prototype.generate = function() {
+	var table = document.createElement("table");
 
+	for (var i = 0; i < this.height; i++) {
+		var row = document.createElement("tr");
+
+		for (var j = 0; j < this.width; j++) {
+			var cell = document.createElement("td");
+
+			this.cells.push(cell);
+		}
+	}
 };
 
 function Sudoku() {
