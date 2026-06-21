f26bf061c609fcde44b2c54e61fd2ff0e03ceb1f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 17:44:57 +0200

    we don't ened to set the height anymore hten that makes them fight

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1491,7 +1491,7 @@ function DragAction() { // This looks less elegant than checking on mouse move b
         function(dialog, offset, difference){ dialog.height = offset.height + difference.y }, // Bottom
         function(dialog, offset, difference){ dialog.left = offset.startX + difference.x; }, // Left
         function(dialog, offset, difference){ dialog.top = offset.startY + difference.y, dialog.left = offset.startX + difference.x; }, // Top Left
-        function(dialog, offset, difference){ dialog.width = offset.width + difference.x, dialog.height = offset.height - difference.y, dialog.top = offset.startY + difference.y },// Top right
+        function(dialog, offset, difference){ dialog.width = offset.width + difference.x, dialog.top = offset.startY + difference.y },// Top right
         function(dialog, offset, difference){ dialog.height = offset.height + difference.y, dialog.width = offset.width + difference.x }, // Bottom right
         function(dialog, offset, difference){ dialog.left = offset.startX + difference.x, dialog.width = offset.width - difference.x, dialog.height = offset.height + difference.y }, // Bottom cleft?
     ];
