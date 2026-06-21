2b8ae3c61bca58f004a95c7b20dc35d53d04c614
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 16:52:41 +0200

    aw

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1359,7 +1359,7 @@ Dialog.prototype.createPopout = function() {
 		var width = self._popupWindow.innerWidth || self._popupWindow.outerWidth, height = self._popupWindow.innerHeight || self._popupWindow.outerHeight;
 
 		if (outerX !== prevRect.x || outerX !== prevRect.y) {
-			var x = outerX - window.screenX,
+			var x = outerX - window.screenX + 10,
 				y = outerY - window.screenY + windowChromeHeight;
 
 			console.log("pos:", self._popupWindow.screenX, self._popupWindow.screenY);
