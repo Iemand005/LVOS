3d14f00fbb60bdcb1dcd78d481b60b138baa9665
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-21 15:04:48 +0200

    you too controlling

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -512,7 +512,7 @@ Dialog.prototype.initWithObject = function(object) {
 							cancelDomEvent(ev);
 							console.log(ev.type, ev.pointerType);
 							// ev.pointerType = "";
-							if (ev.target && isElement(ev.target)) dragAction.set(Number(ev.target.id));
+							if (ev.target && isElement(ev.target)) dragAction.set(id);
 							activationHandler(ev);
 						}
 					}(id); // You can also put index + 1 in here instead for optimal efficiency and minimalism, but Internet Explorer is not a very stubborn browser but netscape is and does not instantiate the index variable but keeps one in memory resulting in resize direction being 9. Despite this it uses very little memory compared to Firefox and Chrome?
