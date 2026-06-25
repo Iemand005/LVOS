8c0c55af2d30eb4f8649fefc8a10d7234c4b5a39
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-25 14:08:07 +0200

    or that huh

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -812,8 +812,8 @@ Object.defineProperty(Dialog.prototype, "maxAspectRatio", {
 Object.defineProperty(Dialog.prototype, "top", {
     get: function() { return this.y; },
     set: function(top) {
-        var newHeight = this.y - top  + this.height;
-        if (newHeight < this.minHeight) {
+        var newHeight = this.y - top + this.height;
+        if (newHeight < this.minHeight || newHeight > this.maxHeight) {
             this.y = this.bottomFromTop - this.minHeight;
             this.height = this.minHeight;
         } else {
