7f0b84f047a18a810773a98258ae15864fb11fb3
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-28 05:29:51 +0200

    resize code for dialog lite

--- Scripts/WindowManagerLite.js
+++ Scripts/WindowManagerLite.js
@@ -1,6 +1,10 @@
 
-function Dialog() {
-
+function Dialog(target) {
+    this.target = target;
 }
 
-alert("IM GOOD")
+alert("IM GOOD");
+
+Dialog.prototype.resize(width, height) {
+    target.resizeTo(width, height);
+}
