4c39e7e62439d373bd988d37d79517f3e4765bf8
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-30 20:46:19 +0200

    else do the individual prps

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1365,7 +1365,15 @@ Dialog.prototype.moveBody = function (x, y) {
  */
 Dialog.prototype.setInset = function(top, left, right, bottom) {
     if (!this.target) return;
-    this.target.style.inset = toPixels(top) + toPixels(left) + toPixels(right) + toPixels(bottom);
+    if (this.target.style.inset) this.target.style.inset = toPixels(top) + toPixels(left) + toPixels(right) + toPixels(bottom);
+    else {
+        this.target.style.top = toPixels(this.top);
+		this.target.style.left = toPixels(this.left);
+        if (!this.useScale) {
+            this.target.style.right = toPixels(this.right);
+            this.target.style.bottom = toPixels(this.bottom);
+        }
+    }
 }
 /** @param {string} url */
 Dialog.prototype.openUrl = function(url) {
