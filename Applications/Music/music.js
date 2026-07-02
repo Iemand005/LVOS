9a53ebb1399fb5fc860fb91b847a89d038145396
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 22:35:45 +0200

    no wait that one is swapped thats why it was wrong at first

--- Applications/Music/music.js
+++ Applications/Music/music.js
@@ -158,7 +158,7 @@ MusicApp.prototype.animateFrame = function(time) {
 
     const fillStyle = "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
 
-    if (this.visualizer === "bars") {
+    if (this.visualizer === "circle") {
         /*let*/var rad = 0, inc = Math.PI*2*(1/count);
         ctx.lineWidth = 100;
         for(let index in timeData){
@@ -193,7 +193,7 @@ MusicApp.prototype.animateFrame = function(time) {
 }
 
 if (visualiserOption) visualiserOption.onchange = function() {
-	if (visualiserOption instanceof HTMLSelectElement) musicApp.visualizer = visualiserOption.selectedIndex === 1 ? "bars" : "circle";
+	if (visualiserOption instanceof HTMLSelectElement) musicApp.visualizer = visualiserOption.selectedIndex === 1 ? "circle" : "bars";
 }
 
 function startAnimation(){
