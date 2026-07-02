69bdef990f87db266acb6d4b444143212eaac632
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 21:59:16 +0200

    imma bring back uh cr obj orented

--- Applications/Music/music.js
+++ Applications/Music/music.js
@@ -41,6 +41,14 @@ if (visualiser instanceof HTMLCanvasElement) {
 
 
 
+/** @param {HTMLCanvasElement} visualizerElement  */
+function MusicApp(visualizerElement) {
+	this.graphics = 
+	// this.ctx = visualizerElement.getContext("2d");
+}
+
+const musicApp = new MusicApp(visualiser);
+
 if (micButton)
 micButton.onclick = function(ev){
     // if (isActive) stop();
