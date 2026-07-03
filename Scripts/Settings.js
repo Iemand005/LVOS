1a7c7f6724b2ddbb225c183b2033e7112aca5d0f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-03 13:41:28 +0200

    monet to the enemy

--- Scripts/Settings.js
+++ Scripts/Settings.js
@@ -77,7 +77,7 @@ function setThemeOld(id) {
 function toggleCharmsEvent(ev) {
     if (!document.elementFromPoint) return;
     var clickedElement = document.elementFromPoint(ev.clientX, ev.clientY);
-    if (!isCharmsOpen() || clickedElement == charmsbutton || clickedElement == charmsbutton2) return;
+    if (!isCharmsOpen() || clickedElement == charmsbutton) return;
 
     if(!(clickedElement == bodyCrawler.charms || bodyCrawler.charms.contains(clickedElement))) {
         //if(clickedElement == charmsbutton || clickedElement == charmsbutton2) toggleCharms();
