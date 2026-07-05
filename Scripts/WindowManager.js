af7835d68e87f2bba2e45afa2552cd40f3ac6a3b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-05 14:25:26 +0200

    beep beep anoan ster

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1167,7 +1167,7 @@ Dialog.prototype.toggleClassAnimated = function (className, force, onTransitionE
 		var animationHandler = function(event) {
 			if (onTransitionEnd && !onTransitionEnd(event.propertyName) || !target) return;
 			dialog.stopAnimating();
-			console.log("Aborting animation over " + event.propertyName + ".", "Took: ", event.elapsedTime, "seconds. Reported by: ", event.target);
+			console.log("Aborting animation over " + event.propertyName + ". Took: ", event.elapsedTime, "seconds. Reported by: ", event.target);
 			target.removeEventListener(transitionEndEvent, animationHandler, false);
 			if (onEnd) onEnd();
 		};
