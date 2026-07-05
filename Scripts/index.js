fc0e9ccab6945e342d43d9dd64fc3bf7b5169069
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-05 13:28:46 +0200

    onload

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,10 @@
 
 var launchpad = new Launchpad();
 
+function init() {
+	windowManager.forEachWindow(function(dialog) {
+		if (dialog.application) launchpad.addApp(dialog.application);
+	});
+}
 
-
-windowManager.forEachWindow(function(dialog) {
-	if (dialog.application) launchpad.addApp(dialog.application);
-});
+window.addEventListener("load", init, false);
