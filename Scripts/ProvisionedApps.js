721b71ccf91966ae82382e94ad7ab5835bbc3083
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-31 01:41:14 +0200

    that's rigte

--- Scripts/ProvisionedApps.js
+++ Scripts/ProvisionedApps.js
@@ -213,7 +213,8 @@ var applications = [
 	{
 		title: "Tappy",
 		id: "foxyz.tappy",
-		src: "https://iemand005.github.io/Tappy/tappy.html",
+		// src: "https://iemand005.github.io/Tappy/tappy.html", // TODO: perhaps uh a prod vs dev env for github url and local
+		src: "./Applications/Cyanide/metronome/index.html",
 		hidden: true
 	}
 ]
