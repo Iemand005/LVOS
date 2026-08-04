d129a6b2253f8bf8455b1e6e4c2e40564ea99bc7
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-04 14:40:07 +0200

    hmemm

--- Applications/Music/music.js
+++ Applications/Music/music.js
@@ -65,7 +65,10 @@ function MusicApp(visualizerElement) {
  * @param {number} b 0-255
  */
 MusicApp.prototype.setAuraColor = function (r, g, b) {
-    if (aura && aura.device) aura.setColor(r, g, b).catch(function () {});
+    if (aura && aura.device) {
+        console.log("Setting aura color from wasm:", r, g, b);
+        aura.setColor(r, g, b).catch(function () {});
+    }
 };
 
 if (visualiser instanceof HTMLCanvasElement) {
