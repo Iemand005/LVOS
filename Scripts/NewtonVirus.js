5e364da9e256f6fd94decb87975dd2adf8e26899
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-04 20:01:25 +0200

    animationframe reqs

--- Scripts/NewtonVirus.js
+++ Scripts/NewtonVirus.js
@@ -4,10 +4,14 @@ function NewtonManager() {
 	this.lastTime = 0;
 }
 
+NewtonManager.prototype.start = function () {
+	requestAnimationFrame(this.start);
+};anim
+
 NewtonManager.prototype.step = function() {
 	var currentTime = Date.now();
 	var deltaTime = this.lastTime - currentTime;
 	this.lastTime = currentTime;
 
-	
+
 };
