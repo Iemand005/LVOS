f837baefcb3173fd6a6d8a215207b4f99679e952
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-04 19:51:01 +0200

    do I even need to deg func bleh whatever

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -705,7 +705,7 @@ function translateElement(element, x, y, skew, scaleY, rotation) {
     var transform = "translate(" + toPixels(x) + "," + toPixels(y) + ")";
     if (skew) transform += " skewX(" + toDegree(skew) + ")";
     if (scaleY) transform += "scaleY(" + scaleY + ")";
-    if (rotation) transform += "rotation(" + scaleY + ")";
+    if (rotation) transform += "rotate(" + rotation + "deg)";
     element.style.transform = transform;
     element.style.webkitTransform = transform;
 }
