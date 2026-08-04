f4cf4a5cee7ae43b23752a934e053d195c7f3016
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-04 19:45:23 +0200

    dt calc for physics sim

--- Scripts/NewtonVirus.js
+++ Scripts/NewtonVirus.js
@@ -1,9 +1,9 @@
 
 
 function NewtonManager() {
-
+	this.lastTime = 0;
 }
 
-NewtonManager.prototype.step = function(step) {
-
+NewtonManager.prototype.step = function() {
+	var deltaTime = this.lastTime - step;
 };
