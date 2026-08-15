d6ce7b5071c60d6c628d9600b76ef3264e19cef0
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-16 00:05:07 +0200

    proxy favicon tets

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -284,7 +284,7 @@ WindowManager.prototype.installAppProxied = function (url, proxyUrl) {
 		src: proxyUrl + url,
 		id: "custom." + getDomain(url),
 		title: getSiteName(url),
-		iconUrl: getFaviconUrl(url)
+		iconUrl: proxyUrl + getFaviconUrl(url)
 	};
 	this.loadApp(application);
 };
