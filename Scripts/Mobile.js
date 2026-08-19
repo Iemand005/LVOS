930ae333cc8e8d7502f3e3467f4b495635490aea
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-19 11:24:32 +0200

    nice helping

--- Scripts/Mobile.js
+++ Scripts/Mobile.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", function(e){
 	const homeButton = document.getElementById("home-button");
+	const backButton = document.getElementById("back-button");
+	const appsButton = document.getElementById("apps-button");
 
 	if (homeButton) homeButton.onclick = function() {
 		var mainFrame = document.getElementById("main-frame");
 		if (!(mainFrame instanceof HTMLIFrameElement)) return;
 		mainFrame.classList.remove("open");
 	};
+
+	if (backButton) backButton.onclick = function() {};
 });
