5efdaad1d73d43b111aa3374a2b665741aba2eef
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-19 11:20:56 +0200

    close the mainframe on home

--- Scripts/Mobile.js
+++ Scripts/Mobile.js
@@ -1,6 +1,9 @@
 window.addEventListener("DOMContentLoad", function(e){
 	const homeButton = document.getElementById("home-button");
 
-	homeButton && homeButton.onclick = function() {
+	if (homeButton) homeButton.onclick = function() {
+		var mainFrame = document.getElementById("main-frame");
+		if (!(mainFrame instanceof HTMLIFrameElement)) return;
+		mainFrame.classList.remove("open");
 	};
 });
