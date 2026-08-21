3c1121ea7a1f50da6fab3a8957d582a019293744
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-22 01:57:38 +0200

    Tabls please

--- eslint.config.js
+++ eslint.config.js
@@ -3,27 +3,27 @@
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  files: ['**/*.ts', '**/*.tsx', '*.d.ts'],
-  languageOptions: {
-    parser: tseslint.parser,
-    parserOptions: {
-      project: './tsconfig.json',
-    },
-  },
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-  },
-  rules: {
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'ArrowFunctionExpression',
-        message: 'Arrow functions are not allowed. Use a regular function instead.',
-      },
-      {
-        selector: 'ChainExpression',
-        message: 'Optional chaining (?.) is not allowed.',
-      },
-    ],
-  },
+	files: ['**/*.ts', '**/*.tsx', '*.d.ts'],
+	languageOptions: {
+		parser: tseslint.parser,
+		parserOptions: {
+			project: './tsconfig.json',
+		},
+	},
+	plugins: {
+		'@typescript-eslint': tseslint.plugin,
+	},
+	rules: {
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'ArrowFunctionExpression',
+				message: 'Arrow functions are not allowed. Use a regular function instead.',
+			},
+			{
+				selector: 'ChainExpression',
+				message: 'Optional chaining (?.) is not allowed.',
+			},
+		],
+	},
 });
