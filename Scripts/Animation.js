8822ae5605415dd3e229b18dbfba659f7e4e28a1
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-21 22:04:17 +0200

    cleaner not qyuite ts langseerver but ok make  it objective static obj functions for jshint not cuz it's better or anyting...

--- Scripts/Animation.js
+++ Scripts/Animation.js
@@ -26,34 +26,34 @@ Animation.animate = function(duration, update, complete) {
     }
 
     tick();
-}
+};
 /**
  * @param {number} a 
  * @param {number} b 
  * @param {number} t 
  * @returns 
  */
-function lerp(a, b, t) {
+Animation.lerp = function(a, b, t) {
     return a + (b - a) * t;
-}
+};
 /** @param {number} t */
-function ease(t) {
+Animation.ease = function(t) {
     return t * t * (3 - 2 * t);
-}
+};
 /** @param {number} t */
-function easeOutLog(t) {
+Animation.easeOutLog = function(t) {
     return Math.log(1 + 9 * t) / Math.log(10);
-}
+};
 /** @param {number} t */
-function easeSharpCenter(t) {
+Animation.easeSharpCenter = function(t) {
     return 0.5 - 0.5 * Math.cos(Math.PI * t);
-}
+};
 /** @param {number} t */
-function easeSharpCenterStrong(t) {
+Animation.easeSharpCenterStrong = function(t) {
     t = 0.5 - 0.5 * Math.cos(Math.PI * t);
     return 0.5 - 0.5 * Math.cos(Math.PI * t);
-}
+};
 /** @param {number} t */
-function easeSharpMiddle(t) {
+Animation.easeSharpMiddle = function(t) {
     return 0.5 + (t - 0.5) * Math.abs(2 * t - 1);
-}
+};
