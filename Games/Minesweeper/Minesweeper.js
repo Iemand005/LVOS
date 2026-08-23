68a3c3724dc4fffaccf2f38a26b0e47b3a4c6bbe
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-23 06:44:40 +0200

    its likea ddeventslisterenr and evenat handler hmm

--- Games/Minesweeper/Minesweeper.js
+++ Games/Minesweeper/Minesweeper.js
@@ -197,7 +197,7 @@ function sendDesiredSize(){
 	try {
 		var form = document.querySelector("form");
 		if (!form) return;
-		LVMessenger.broadcastToParent(LVMessenger.types.windowSize, {width: form.offsetWidth, height: form.offsetHeight}, "minesweeper"); // Broken tooth 23/08/2026.
+		LVMessenger.broadcastToParent("windowSize", {width: form.offsetWidth, height: form.offsetHeight}, "minesweeper"); // Broken tooth 23/08/2026.
 	} catch(ex) {
 		console.error("Failed to post desired size", ex);
 	}
