b4e0aa51e4e8a46a74baa6b5281f92bdd55466da
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-24 21:19:07 +0200

    I mean if it isundef

--- Scripts/DesktopManager.js
+++ Scripts/DesktopManager.js
@@ -83,7 +83,7 @@ var onLoad = function () {
 
     if  (launchpad) launchpad.open();
 
-    if (!ClickOffset) return;
+    if (typeof ClickOffset !== "undefined") return;
 
 	var clickOffset = new ClickOffset;
 
