d87a202be2007df58357309feda1675a539789d9
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-30 23:24:30 +0200

    not so lost huh

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -2372,7 +2372,8 @@ function toggleOverlay(enable) {
 	overlay.classList.toggle("open", enable);
 }
 
-function initializeDialogs() {
+/** @deprecated  I think this one isn't used anymeore??*/
+WindowManager.prototype.ininializeDialogs = function() {
     if (supportsPointer) {
         document.onpointerup = windowManager.disableDialogDrag;
         window.onpointerup = windowManager.disableDialogDrag;
