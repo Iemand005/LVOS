93d3d8b8a827a834a60ee45b33ecc3ca22144f2d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-31 18:54:34 +0200

    ok use id val instead of idx+1

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1188,19 +1188,19 @@ Dialog.prototype.initWithObject = function(object) {
             var createSizer = function (id) {
 				if (!target) return;
 
-                var sizerId = "sizer-" + (index + 1);
+                var sizerId = "sizer-" + (id);
 
                 var div = this.getElementByTagOrClassName(sizerId);
                 if (!div || !(isElement(div))) div = document.createElement("div");
                 div.draggable = false;
-				div.id = String(index + 1);
+				div.id = String(id);
 				div.classList.add(sizerId);
                 /** @type {(this: GlobalEventHandlers, ev: PointerEvent | MouseEvent) => any} */
                 var pointerDown = function (ev) {
                     cancelDomEvent(ev);
                     if (ev.target && "id" in ev.target) windowManager.dragAction.set(Number(ev.target.id));
                     activationHandler(ev);
-                }; // You can also put index + 1 in here instead for optimal efficiency and minimalism, but Internet Explorer is not a very stubborn browser but Netscape is and does not instantiate the index variable but keeps one in memory resulting in resize direction being 9. Despite this it uses very little memory compared to Firefox and Chrome?
+                }; // You can also put id in here instead for optimal efficiency and minimalism, but Internet Explorer is not a very stubborn browser but Netscape is and does not instantiate the index variable but keeps one in memory resulting in resize direction being 9. Despite this it uses very little memory compared to Firefox and Chrome?
                 if (supportsPointer) div.onpointerdown = pointerDown;
                 else div.onmousedown = pointerDown;
                 target.appendChild(div);
