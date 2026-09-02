187fc7affae1ac4c1dfe12ca3b95bb4a2284a621
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-02 06:54:33 +0200

    oknice

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1004,7 +1004,7 @@ function Dialog(object, create) {
 	/** @readonly */
 	this._bodyOffset = { width: 0, height: 0, x: 0, y: 0 };
 
-	/** @type {{_fsTimeout: number?, _fsRaf: number?, _fsToken: number?, _fsTokenAtStart: number? }} */
+	/** @type {{_fsTimeout: number?, _fsRaf: number?, _fsToken: number?, _fsTokenAtStart: number? }} @readonly */
 	this._animationProps = { _fsTimeout: 0, _fsRaf: null, _fsToken: null, _fsTokenAtStart: null };
 
     if (!object) return;
