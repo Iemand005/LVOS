d88ba5c603f3ee5bd11b71a2f13a079a9f592dea
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-02 04:41:01 +0200

    gravy ok

--- Scripts/DesktopManager.js
+++ Scripts/DesktopManager.js
@@ -62,7 +62,7 @@ var onLoad = function () {
 
 		var updateClock = function() {
 			// clock.innerHTML = new Date().toLocaleTimeString();
-            $0.dateTime =a.toISOString();
+            clock.dateTime = new Date().toISOString();
 		};
 
 		updateClock();
