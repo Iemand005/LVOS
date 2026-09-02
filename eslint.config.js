886ecbda670d633bb17d046ce0e25ada470b7ea7
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-02 05:28:57 +0200

    should I suffix es6 files

--- eslint.config.js
+++ eslint.config.js
@@ -1,62 +1,85 @@
-/* jshint esversion: 6 */
-
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 import jsdoc from "eslint-plugin-jsdoc";
 
-export default tseslint.config({
-	files: ["**/*.js", "**/*.ts", "**/*.tsx", "*.d.ts"],
-	languageOptions: {
-		parser: tseslint.parser,
-		parserOptions: {
-			project: "./jsconfig.json"
+export default defineConfig(
+	{
+		files: ["**/*.js", "**/*.ts", "**/*.tsx", "*.d.ts"],
+
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: "./jsconfig.json"
+			},
+			globals: {
+				...globals.browser,
+				...globals.commonjs,
+				Anim: "readonly",
+				Vector: "writable",
+				LVMessenger: "writable",
+				ClickOffset: "writable",
+				lerp: "writable",
+				windowManager: "readonly",
+				toggleReflections: "writable",
+				setTheme: "writable",
+				WindowManager: "readonly",
+				Dialog: "readonly"
+			}
 		},
-		globals: {
-			...globals.browser,
-			...globals.commonjs,
-			Anim: "readonly",
-			Vector: "writable",
-			LVMessenger: "writable",
-			DesktopManager: "writable",
-			ClickOffset: "writable",
-			lerp: "writable",
-			windowManager: "readonly",
-			toggleReflections: "writable",
-			setTheme: "writable",
-			WindowManager: "readonly",
-			Dialog: "readonly"
+
+		plugins: {
+			"@typescript-eslint": tseslint.plugin,
+			jsdoc
+		},
+
+		rules: {
+			...jsdoc.configs["flat/recommended"].rules,
+			"comma-dangle": ["error", "never"],
+			"no-var": "off",
+			"no-sequences": "off",
+			"comma-spacing": "warn",
+			"no-trailing-spaces": "warn",
+			"semi": ["warn", "always"],
+			"quotes": ["warn", "double"],
+			"no-unused-vars": "warn",
+			"no-undef": "error",
+			"eqeqeq": "warn",
+
+			"no-restricted-syntax": [
+				"error",
+				{
+					selector: "ArrowFunctionExpression",
+					message: "Arrow functions are not allowed. Use a regular function instead."
+				},
+				{
+					selector: "ChainExpression",
+					message: "Optional chaining (?.) is not allowed."
+				},
+				{
+					selector: "CallExpression[callee.property.name='addEventListener'][arguments.length=2]",
+					message: "addEventListener requires a third options parameter for Netscape."
+				}
+			]
 		}
 	},
-	plugins: {
-		"@typescript-eslint": tseslint.plugin,
-		jsdoc
-	},
-	rules: {
-		...jsdoc.configs["flat/recommended"].rules,
-		"comma-dangle": ["error", "never"],
-		"no-var": "off",
-		"no-sequences": "off",
-		"comma-spacing": "warn",
-		"no-trailing-spaces": "warn",
-		"semi": ["warn", "always"],
-		"quotes": ["warn", "double"],
-		"no-unused-vars": "warn",
-		"no-undef": "error",
-		"eqeqeq": "warn",
-		"no-restricted-syntax": [
-			"error",
-			{
-				selector: "ArrowFunctionExpression",
-				message: "Arrow functions are not allowed. Use a regular function instead."
-			},
-			{
-				selector: "ChainExpression",
-				message: "Optional chaining (?.) is not allowed."
-			},
-			{
-				selector: "CallExpression[callee.property.name='addEventListener'][arguments.length=2]",
-				message: "addEventListener requires a third options parameter for Netscape."
-			}
-		]
+
+	// ES6 files: allow arrow functions
+	{
+		files: ["**/*.es6.js"],
+
+		rules: {
+			"no-restricted-syntax": [
+				"error",
+				{
+					selector: "ChainExpression",
+					message: "Optional chaining (?.) is not allowed."
+				},
+				{
+					selector: "CallExpression[callee.property.name='addEventListener'][arguments.length=2]",
+					message: "addEventListener requires a third options parameter for Netscape."
+				}
+			]
+		}
 	}
-});
+);
