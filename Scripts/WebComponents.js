ea38a8be05cac1286e175d0ae3506ed105e65538
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-02 05:38:57 +0200

    set myself's attrib for me!

--- Scripts/WebComponents.js
+++ Scripts/WebComponents.js
@@ -3,7 +3,9 @@ class OdometerTrack extends HTMLSpanElement {
 		super();
 		this.lineHeight = 40;
 	}
-	connectedCallback() {}
+	connectedCallback() {
+		this.setAttribute("is", "odometer-track");
+	}
 	/** @param {number} digit Digit value */
 	setDigit(digit) {
 		const shiftY = digit * this.lineHeight;
@@ -47,7 +49,6 @@ class OdometerTime extends HTMLTimeElement {
 		const track = document.createElement("span", {
 			is: "odometer-track"
 		});
-		track.setAttribute("is", "odometer-track");
 		track.textContent = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9";
 		if (track instanceof OdometerTrack) this.tracks.push(track);
 		this.appendChild(track);
