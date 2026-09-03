1e1f4a2282a87a4c19668e53f102230d2d75721b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-03 13:54:38 +0200

    let it throw let it throw!!

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -239,7 +239,7 @@ function messageReceived(type, data, source){
 			case "pip":
 				var targetElement = dialog.getElementById(data.id);
 				console.log("Ripped out element:", targetElement);
-				if (!targetElement || !DesktopManager.toggleElementPip) break;
+				if (!targetElement) break;
 				DesktopManager.toggleElementPip(targetElement, function (pipWindow) {
 					if (!pipWindow) return;
 					pipWindow.onresize = function() {
