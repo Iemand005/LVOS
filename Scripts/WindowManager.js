7247cd91f0cddfb4abef9879a04bfb694da513a4
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-05 16:48:22 +0200

    why is the thisarg propertydesc

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -1203,6 +1203,7 @@ Dialog.prototype.initWithObject = function(object) {
 };
 
 Object.defineProperty(Dialog.prototype, "isOpen", {
+	/** @this {Dialog} */
 	get: function() { return Boolean(this.target && this.target.classList.contains("open")); },
 	set: function(open) { this.toggleOpen(open); }
 });
