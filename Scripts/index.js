61d99acfa220847101becb76207e7c51f7dd50da
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-09 19:34:28 +0200

    flabber

--- Scripts/index.js
+++ Scripts/index.js
@@ -37,13 +37,6 @@ function init() {
 		});
 	}
 
-	var modules = document.querySelectorAll('script[type="module"]');
-	for (var s in modules) {
-		var replacement = document.createElement('script');
-		replacement.src = modules[s].src;
-		modules[s].replaceWith(replacement);
-	}
-
 	window.metaThemeColor = document.querySelector("meta[name=\"theme-color\"]") || undefined;
 	if (window.__LVMessenger)
 		window.__LVMessenger.accent = window.metaThemeColor;
