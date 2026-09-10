609e1f45676f3fea9aa388e54b6f67030317a18e
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-10 23:09:29 +0200

    jason

--- Scripts/ProvisionedApps.js
+++ Scripts/ProvisionedApps.js
@@ -251,7 +251,7 @@ var applications = [
 		id: "lvos",
 		src: "index.html",
 		distSrc: "https://iemand005.github.io/LVOS",
-		altUrls: ["https://iemand005.github.io/LVOS-dist/", "https://localhost:5000", "https://localhost:5001", "https://lvos.neocities.org"]
+		altUrls: ["https://iemand005.github.io/LVOS-dist", "https://localhost:5000", "https://localhost:5001", "https://lvos.neocities.org"]
 	}
 ];
 
