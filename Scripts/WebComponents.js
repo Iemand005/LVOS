c42e9f9a182d21deb7b2c337827283b91b03ff87
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-10 23:57:43 +0200

    gvalue getter then

--- Scripts/WebComponents.js
+++ Scripts/WebComponents.js
@@ -40,10 +40,11 @@ class WindowElement extends HTMLDivElement {
 }
 
 class OdometerDigit extends HTMLSpanElement {
-	constructor()  { super(); this.lineHeight = 40; }
+	constructor()  { super(); this.lineHeight = 40, this._value = 0; }
 	connectedCallback() { this.setAttribute("is", "odometer-track"); }
 	/** @param {number} digit Digit value */
 	set value(digit) { this.style.transform = `translateY(-${digit * this.lineHeight}px)`; }
+	get value() { return this._value;}
 }
 
 class OdometerDisplay extends HTMLTimeElement {
