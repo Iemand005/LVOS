dc2b6911e65587eaba9ce62202a1634cacdb6dbb
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-10 17:01:40 +0200

    oknice

--- Scripts/Mobile.js
+++ Scripts/Mobile.js
@@ -25,24 +25,7 @@ window.addEventListener("load", function(e){
 		goBack();
 	});
 
-	const blurGradient = document.querySelector('.blur-gradient');
-
-/*
-const layers = 100;
-const maxBlur = 10;
-const exponent = 2;
-
-for (let i = layers; i > 0; i--) {
-	const element = document.createElement('div');
-	const t = (i - 1) / (layers - 1);
-
-	element.style.setProperty('--blur', `${maxBlur * t ** exponent}px`);
-	element.style.setProperty('--start', `${(layers - i) / layers * 100}%`);
-	element.style.setProperty('--end', `${(layers - i + 1) / layers * 100}%`);
-
-	blurGradient.appendChild(element);
-}
-	*/
+	const blurGradient = this.document.createElement("div");
 
 	const layers = 10;
 	const blur = 0.5;
