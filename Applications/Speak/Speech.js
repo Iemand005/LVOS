292f3ad7c4af0f9796725cededfae8d90252872a
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-19 22:41:32 +0200

    if if thos

--- Applications/Speak/Speech.js
+++ Applications/Speak/Speech.js
@@ -1,9 +1,14 @@
 function Speach() {
-	
+	this.volume = 1;
+	this.speed = 1;
+	this.pitch = 1;
 }
 
 Speach.prototype.say = function(msg) {
 	var utterance = new SpeechSynthesisUtterance("Hello world");
+	utterance.rate = this.speed;
+	utterance.volume = this.volume;
+	utterance.pitch = this.pitch;
 	speechSynthesis.speak(utterance);
 };
 
