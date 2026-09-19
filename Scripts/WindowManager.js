bb5cb2ed1c547b0c91b2e6d06a496813aaac5820
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-19 02:20:53 +0200

    hrmm yes thats netter

--- Scripts/WindowManager.js
+++ Scripts/WindowManager.js
@@ -801,6 +801,7 @@ WindowManager.prototype.broadcast = function() {
 	this.channel.postMessage({
 		type: "Hi"
 	})
+	LVMessenger.broadcast(this.channel, "window-move", {x: 2});
 }
 
 //#endregion
@@ -2001,8 +2002,6 @@ Dialog.prototype.toggleFullButton = function (enable) {
 Dialog.prototype.messageFrame = function (type, message) {
 	var frame = this.frame;
 	if (frame) LVMessenger.broadcastToChild(type, frame, message);
-	var target = this.target;
-	if (frame && frame.contentWindow) frame.contentWindow.postMessage({type: type, data: message}, "*");
 };
 Dialog.prototype.updateTranslation = function () {
 	if (this.useTransform && this.target) translateElement(this.target, this._maximizing ? 0 : this.x, this._maximizing ? 0 : this.y, this._skew, this._scaleX, this._scaleY, this._rotation);
